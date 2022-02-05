7f611871d32a3601d39d37e1b5e09c1e6c3ce7f4
Latif Essam <latifekhaliel@gmail.com>
2022-02-05 20:21:07 +0200

    finished second module

--- api/app.js
+++ api/app.js
@@ -1,3 +1,10 @@
-const app = require("express");
-const pathToIndex;
-export default app;
+const app = require('express');
+const path = require('path');
+const pathToIndex = path.resolve(__dirname, '../client/index.html');
+app.use('/*', (req, res) => {
+    res.sendFile(pathToIndex);
+})
+
+
+module.exports = app;
+
